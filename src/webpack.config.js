2abe12342c1f4ad5b950d32da3a269971ad44353
ShreoshreeSingha <102968693+ShreoshreeSingha@users.noreply.github.com>
2023-03-28 16:58:19 +0530

    update configue

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,8 +1,23 @@
+const path = require('path');
+
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
