0f6b572414ad680a4fdc71e6a85ffbc087cd1bc9
Debanjan <debauv.voc77@gmail.com>
2023-03-29 10:24:13 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import "./index.css";
-// import App from "./App";
-
-// const root =ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//       <App />
-// );
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+// eslint-disable-next-line import/no-unresolved
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ContextProvider } from './contexts/ContextProvider';
-import { AuthContextProvider } from './contexts/auth-context';
+// eslint-disable-next-line import/no-unresolved
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <AuthContextProvider>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </AuthContextProvider>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
