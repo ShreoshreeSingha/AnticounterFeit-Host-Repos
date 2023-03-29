1dfb13ad75a72e269bbeabe32ba09ce8dcc6b66d
Debanjan <debauv.voc77@gmail.com>
2023-03-29 10:14:39 +0530

    debugged error

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path');
+import { resolve } from 'path';
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+export const entry = './src/index.js';
+export const output = {
+  filename: 'bundle.js',
+  path: resolve(__dirname, 'build'),
+};
+export const module = {
+  rules: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
-    ],
-  },
+    },
+  ],
 };
