13402f1f777ba9d2c1656e0e8142209b025f10a0
rahulkujur <rahul.kujur@acespireconsulting.com>
2023-03-31 15:24:46 +0530

    Babel file updated

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
