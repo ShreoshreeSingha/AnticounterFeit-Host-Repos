f3d805bfbb2a383db28dec7bf9f63e7bf0f4267b
Debanjanmaj <106948081+Debanjanmaj@users.noreply.github.com>
2023-03-31 14:00:08 +0530

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   resolve: {
     extensions: ["*", ".js", ".jsx"],
     alias: {
-      components: path.resolve(__dirname, "src/components"),
+      components: path.resolve(__dirname, "src/components/"),
     },
   },
   plugins: [
